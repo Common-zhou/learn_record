e6d0770cfc887935fac98134eaf42e74c906083b
zhoubing <1050015785@qq.com>
2022-03-26 20:47:30 +0800

    练习bio

---
+++ nio/src/main/java/com/zhou/HttpServer03.java
@@ -0,0 +1,52 @@
+package com.zhou;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 多线程去处理
+ * sb -u http://localhost:8802 -c 20 -N 60
+ *
+ * @author zhoubing
+ * @date 2022-03-26 16:31
+ */
+public class HttpServer03 {
+
+    private static ExecutorService executorService = Executors.newFixedThreadPool(32);
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8803);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                service(socket);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void service(Socket socket) {
+        executorService.submit(() -> {
+            try {
+                PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+                printWriter.println("HTTP/1.1 200 OK");
+                printWriter.println("Content-Type:text/html;charset=utf-8");
+                String body = "hello,nio3";
+                printWriter.println("Content-Length:" + body.getBytes().length);
+                printWriter.println();
+                printWriter.println(body);
+                printWriter.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
+}
