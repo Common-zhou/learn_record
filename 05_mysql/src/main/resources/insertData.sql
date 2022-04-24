248eeeeb1007c9683b0a3820e00b211a1007e8e0
zhoubing <1050015785@qq.com>
2022-04-25 00:46:39 +0800

    practice sql

---
+++ 05_mysql/src/main/resources/insertData.sql
@@ -0,0 +1,15 @@
+DROP PROCEDURE IF EXISTS insert_user;
+
+DELIMITER ;;
+CREATE PROCEDURE insert_user()
+BEGIN
+	DECLARE i INT DEFAULT 1;
+WHILE i<1000000 DO
+	set autocommit = 1;
+insert into user(name,password,salt,birthday,create_time,update_time) values(concat('name',i), concat('password', i),'salt',now(),now(),now());
+SET i=i+1;
+END WHILE ;
+commit;
+END ;;
+
+call insert_user();
