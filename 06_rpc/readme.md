9d3eeaff70cb2ef84d2d89d4796317b576b492e5
zhoubing <zhoubing@sensorsdata.cn>
2022-05-18 09:15:20 +0800

    修改部分代码

--- 06_rpc/readme.md
+++ 06_rpc/readme.md
@@ -1,8 +1,8 @@
-1、（选做）实现简单的Protocol Buffer/Thrift/gRPC(选任一个)远程调用demo。
+### 1、（选做）实现简单的Protocol Buffer/Thrift/gRPC(选任一个)远程调用demo。
 
-2、（选做）实现简单的WebService-Axis2/CXF远程调用demo。 
+### 2、（选做）实现简单的WebService-Axis2/CXF远程调用demo。
 
-3、（必做）改造自定义RPC的程序，提交到github： 
+### 3、（必做）改造自定义RPC的程序，提交到github：
 
 1）尝试将服务端写死查找接口实现类变成泛型和反射
 
@@ -10,7 +10,7 @@
 
 3）尝试使用Netty+HTTP作为client端传输方式
 
-4、（选做☆☆）升级自定义RPC的程序：
+### 4、（选做☆☆）升级自定义RPC的程序：
 
 1）尝试使用压测并分析优化RPC性能
 
--- 06_rpc/rpcfx-server/src/main/java/com/zhou/controller/ServerController.java
+++ 06_rpc/rpcfx-server/src/main/java/com/zhou/controller/ServerController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Optional;
 
 /**
  * @author zhoubing
@@ -50,12 +50,7 @@ public class ServerController implements ApplicationContextAware {
             response.setResult(JSON.toJSONString(result, SerializerFeature.WriteClassName));
             response.setStatus(0);
             return response;
-        } catch (IllegalAccessException | InvocationTargetException | BeansException e) {
-            e.printStackTrace();
-            response.setException(e.toString());
-            response.setStatus(100);
-            return response;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             response.setException(e.toString());
             response.setStatus(100);
@@ -65,8 +60,19 @@ public class ServerController implements ApplicationContextAware {
 
     }
 
+    /**
+     * 这个方法目前只能找到方法，但是无法找到对应参数的方法。需要注意
+     * @param bean
+     * @param methodName
+     * @return
+     */
     private Method resolveMethod(Object bean, String methodName) {
-        return Arrays.stream(bean.getClass().getMethods()).filter(method -> method.getName().equals(methodName)).findFirst().get();
+        Optional<Method> optional = Arrays.stream(bean.getClass().getMethods()).filter(
+            method -> method.getName().equals(methodName)).findFirst();
+        if (optional.isPresent()){
+            return optional.get();
+        }
+        throw new IllegalArgumentException(String.format("no such method.[bean=%s, methodName=%s]", bean, methodName));
     }
 
     @Bean(name = "com.zhou.api.service.UserService")
